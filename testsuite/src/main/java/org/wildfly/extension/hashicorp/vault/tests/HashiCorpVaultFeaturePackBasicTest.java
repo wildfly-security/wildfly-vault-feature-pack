7d134b0e7a5177a23f4323f12ce26404f1e541ca
Ken Wills <kwills@redhat.com>
2026-02-02 18:18:16 -0600

    enable basic testsuite

--- testsuite/src/main/java/org/wildfly/extension/hashicorp/vault/tests/HashiCorpVaultFeaturePackBasicTest.java
+++ testsuite/src/main/java/org/wildfly/extension/hashicorp/vault/tests/HashiCorpVaultFeaturePackBasicTest.java
@@ -5,9 +5,15 @@
 
 package org.wildfly.extension.hashicorp.vault.tests;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class HashiCorpVaultFeaturePackBasicTest {
-    
+
+    @Test
     public void testBuild() {
         // does nothing yet, just here to compile
+        assertEquals("test1", 10, 10);
     }
 }
